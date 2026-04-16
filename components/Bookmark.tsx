dfb4df7b09c7d6110a56a994d6320d58e50b7c78
Ethan <46520558+Dev-EthanR@users.noreply.github.com>
2026-04-16 16:11:31 +1000

    add optimistic updates

--- components/Bookmark.tsx
+++ components/Bookmark.tsx
@@ -7,6 +7,7 @@ import { Series } from "@/utils/types/Series";
 import { Movie } from "@/utils/types/Movie";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { Bookmark as BookmarkType } from "@/lib/generated/prisma/client";
 
 interface Props {
   classname?: string;
@@ -29,16 +30,51 @@ const Bookmark = ({
     : "/icon-bookmark-empty.svg";
   const { mutate } = useMutation({
     mutationFn: async (nextState: boolean) => {
-      if (nextState === false && bookmarkId) {
-        return axios.delete(`/api/bookmark/${bookmarkId}`);
+      if (!nextState && bookmarkId) {
+        const res = await axios.delete<BookmarkType>(
+          `/api/bookmark/${bookmarkId}`,
+        );
+        return res.data;
       }
-      return axios.post("/api/bookmark", { ...object, type });
+      const res = await axios.post<BookmarkType>("/api/bookmark", {
+        ...object,
+        type,
+      });
+      return res.data;
     },
-    onError: () => {
+    onMutate: async (nextState: boolean) => {
+      await queryClient.cancelQueries({ queryKey: ["bookmarks"] });
+
+      const previous = queryClient.getQueryData<BookmarkType[]>(["bookmarks"]);
+
+      queryClient.setQueryData<BookmarkType[]>(["bookmarks"], (old = []) => {
+        if (nextState) {
+          return [
+            ...old,
+            {
+              id: "temp",
+              itemId: object.id,
+            } as BookmarkType,
+          ];
+        } else {
+          return old.filter((b) => b.id !== bookmarkId);
+        }
+      });
+
+      return { previous }; // returned as context
+    },
+    onError: (_err, _nextState, context) => {
+      if (context?.previous) {
+        queryClient.setQueryData(["bookmarks"], context.previous);
+      }
       setIsBookmarked((prev) => !prev);
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["bookmarks"] });
+    onSuccess: (data, nextState) => {
+      if (nextState) {
+        queryClient.setQueryData<BookmarkType[]>(["bookmarks"], (old = []) =>
+          old.map((b) => (b.id === "temp" ? data : b)),
+        );
+      }
     },
   });
   const queryClient = useQueryClient();
